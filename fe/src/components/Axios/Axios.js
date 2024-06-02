c737e6f8bbb09b717c6fa1082b46c21aad6d6760
SimoneTerzi <simoneterzi3@gmail.com>
2024-06-02 17:45:50 +0200

    10
    10

--- fe/src/components/Axios/Axios.js
+++ fe/src/components/Axios/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configurazione dell'istanza di axios
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL: "https://thirdiograpy-cupstone.onrender.com",
 });
 
 // Funzioni per il portfolio fotografico
